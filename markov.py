c3500cdb239843eb4a6019928eb7e1685916ea3f
rdpcodes <54380958+rdpfeifle@users.noreply.github.com>
2022-09-27 18:09:50 -0600

    Added Pride Prejudice Zombies file

--- markov.py
+++ markov.py
@@ -104,7 +104,8 @@ def make_text(chains):
     
 
 # input_path = 'green-eggs.txt'
-input_path = 'jokes.txt'
+# input_path = 'jokes.txt'
+input_path = 'pride-prejudice-zombies.txt'
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
